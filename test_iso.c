278ec0183493a9608590e2714b2fbb08cbc1a70c
Byounghoon Lee <lbspresley@gmail.com>
2025-06-13 15:28:39 +0900

    add test code for ACK

--- test_iso.c
+++ test_iso.c
@@ -44,7 +44,7 @@ void test_parse_bokwire_envelope()
     // printf("Empty string case result: %d\n", ret);
 }
 
-void test_xml_operations() 
+void test_build_SecurityHandshake() 
 {
     // 새로운 XML 문서 생성
     xmlDocPtr doc = xmlNewDoc((const xmlChar*)"1.0");
@@ -115,12 +115,99 @@ void test_xml_operations()
     xmlFreeDoc(doc);
 }
 
+void test_build_ACK() 
+{
+    // parameters
+    char *respcd="SUCCESS";
+    char *msgtpcd="pacs.009_CORE";
+    char *bizsvc="bok.rtgs.gtr.01";
+    char * bizmsgidr="202506131518S000000001";
+
+    // 새로운 XML 문서 생성
+    xmlDocPtr doc = xmlNewDoc((const xmlChar*)"1.0");
+    if (doc == NULL) {
+        printf("Failed to create new XML document\n");
+        return;
+    }
+
+    char* namespace = "urn:bok:std:iso:20022:xsd:001";
+    char namespace_prefix[12] = "bwh";
+    char nodeName[512] = "BokwireEnvelope";
+    char parentNode[512] = "";
+
+    // 루트 노드 추가
+    doc = add_root_node(doc, nodeName, namespace, namespace_prefix);
+    if (doc == NULL) {
+        printf("1. Failed to add root node\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    // XPath를 사용하여 노드 추가
+    strcpy(parentNode, "/BokwireEnvelope");
+    doc = add_node_with_value(doc, parentNode, "bwh:BokwireHeader", NULL, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("2. Failed to add BokwireHeader\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    strcat(parentNode, "/bwh:BokwireHeader");
+    doc = add_node_with_value(doc, parentNode, "Response", NULL, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("3. Failed to add Response\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+    printf("parent node: %s\n", parentNode);
+
+    strcat(parentNode, "/Response");
+    doc = add_node_with_value(doc, parentNode, "RespCd", respcd, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("4. Failed to add RespCd\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    doc = add_node_with_value(doc, parentNode, "MsgTpCd", msgtpcd, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("5. Failed to add MsgTpCd\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    doc = add_node_with_value(doc, parentNode, "BizSvc", bizsvc, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("5. Failed to add BizSvc\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    doc = add_node_with_value(doc, parentNode, "BizMsgIdr", bizmsgidr, namespace, namespace_prefix, 1);
+    if (doc == NULL) {
+        printf("6. Failed to add BizMsgIdr\n");
+        xmlFreeDoc(doc);
+        return;
+    }
+
+    // 생성된 XML 출력
+    char* xml = build_xml(doc);
+    if (xml != NULL) {
+        printf("7. Generated XML:\n%s\n", xml);
+        xmlFree(xml);
+    }
+
+    // 메모리 해제
+    xmlFreeDoc(doc);
+}
+
 int main() {
     // libxml2 초기화
     xmlInitParser();
     
     test_parse_bokwire_envelope();
-    test_xml_operations();
+    test_build_SecurityHandshake();
+    test_build_ACK();
     
     // libxml2 정리
     xmlCleanupParser();
