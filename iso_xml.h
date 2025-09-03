3d297980737429ddf224d6586580765e63601e18
Byounghoon Lee <lbspresley@gmail.com>
2025-09-03 15:07:52 +0900

    iconv.h 추가

--- iso_xml.h
+++ iso_xml.h
@@ -17,6 +17,8 @@ xmlDocPtr add_node_xpath(xmlDocPtr doc, const char* xpath, const char* node_name
 xmlDocPtr add_node_with_value(xmlDocPtr doc, const char* parent_xpath_param, const char* node_name, const char* node_value, const char* namespace, const char* namespace_prefix, int exclude_namespace) ;
 void change_local_name(const char* node_name, char* parent_node);
 int extract_namespaces_from_xml(const char* xml, xmlXPathContextPtr xpathCtx);
+#else
+#include <iconv.h>
 #endif
 
 #endif
