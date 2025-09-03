9e44056d1dc15bbd49f64d9b6c78fb83cc25418f
Byounghoon Lee <lbspresley@gmail.com>
2025-09-03 13:58:26 +0900

    source 정리

--- CF_APortConnect.c
+++ CF_APortConnect.c
@@ -33,12 +33,9 @@ int CF_APortConnect(int Type, long* pInfo)
   pResult = (TGL_RESULT*)*pInfo;
   SessCnt = pResult->Count;
 
-
   idx = func_GetSGFirstIdx(pResult->SrcSvc);
   if(idx <= -1)
   {
-    //return -1;
-    //return RC_DROP_ALL; // ??
     return RC_NEXT_ACTION;
   }
 
@@ -67,23 +64,17 @@ int CF_APortConnect(int Type, long* pInfo)
 #endif
 
 #if 1
-    if( (pResult->Result > -1) )
-    {
-      if(pResult->Reason == 7)
-      {
+    if( (pResult->Result > -1) ) {
+      if(pResult->Reason == 7) {
         // 기연결
         ulog(_ERROR_, "[로그정보] 기연결 회선 연결" );
         g_TermTable[i].cnt_stat = 7;
-      }
-      else
-      {
+      } else {
         // 연결
         ulog(_ERROR_, "[로그정보] 신규회선 연결" );
         g_TermTable[i].cnt_stat = 1;
       }
-    }
-    else
-    {
+    } else {
       // 접속실패
       ulog(_ERROR_, "[로그정보] 회선연결 실패" );
       g_TermTable[i].cnt_stat = -1;
@@ -103,10 +94,8 @@ int CF_APortConnect(int Type, long* pInfo)
     sprintf(PeerPort, "%d", pResult->PeerPort);
 
 
-    memset( g_TermTable[i].SGName, 0x00, 
-        sizeof(g_TermTable[i].SGName));
-    strncpy( g_TermTable[i].SGName,
-        pResult->SrcSvc, strlen(pResult->SrcSvc) );
+    memset( g_TermTable[i].SGName, 0x00, sizeof(g_TermTable[i].SGName));
+    strncpy( g_TermTable[i].SGName, pResult->SrcSvc, strlen(pResult->SrcSvc) );
     g_TermTable[i].onuse      = 1;
     g_TermTable[i].stidx      = pResult->stidx; 
     g_TermTable[i].PeerIP     = pResult->PeerIP;
@@ -120,44 +109,26 @@ int CF_APortConnect(int Type, long* pInfo)
 
   for( i=0; i<g_maxterm_cnt; i++ )
   {
-    if( g_TermTable[i].cnt_stat > -1)
-    {
+    if( g_TermTable[i].cnt_stat > -1) {
       RCntCount++;
 
-      if( (g_AutoDisCnt == 1) && (g_TermTable[i].cnt_stat == 1) )
-      {
+      if( (g_AutoDisCnt == 1) && (g_TermTable[i].cnt_stat == 1) ) {
         // On Timer
         lParam = g_TermTable[i].stidx;
         // timerID = TIMERID_DISCNT + lParam;
         timerID = 0;
-        rdf_setTimer(   timerID, g_DisCntInterval*1000, -1,
-            lParam, rParam,
-            CF_DisCntSession    ); 
+        rdf_setTimer( timerID, g_DisCntInterval*1000, -1, lParam, rParam, CF_DisCntSession ); 
 #ifdef _DEBUG_ 
         ulog(_FLOW_, "[로그정보] TIMERID for auto disconnect\n" 
             "[TIMER_ID:%d][lParam:%d]"
             , timerID, lParam); 
 #endif
-      }
-      else
-      {
+      } else {
 #ifdef _DEBUG_
         ulog(_FLOW_, "[로그정보] [stidx:%d] [cnt_stat:%d]" 
             , g_TermTable[i].stidx, g_TermTable[i].cnt_stat);
 #endif
       }
-
-#if 0
-      if( (g_UsePoll == 1) && (g_TermTable[i].cnt_stat == 1) )
-      {
-        // On Timer
-        lParam = g_TermTable[i].stidx;
-        timerID = TIMERID_REQ_POLL + lParam;
-        rdf_setTimer(   timerID, g_ReqPollInterval*1000, -1,
-            lParam, rParam,
-            CF_SendPollReq);
-      }	
-#endif
     }
   }
 
@@ -169,18 +140,13 @@ int CF_APortConnect(int Type, long* pInfo)
 #endif
 
   // 실제로 연결된 세션 수 변경
-  if(g_UpSessLimit > RCntCount) 
-  {
-    if (g_SessStat != 0)
-    {
+  if(g_UpSessLimit > RCntCount) {
+    if (g_SessStat != 0) {
       g_SessStat = 0;
       func_SendChannelStat(0);
     }
-  }
-  else
-  {
-    if (g_SessStat == 0)
-    {
+  } else {
+    if (g_SessStat == 0) {
       g_SessStat = 1;
       func_SendChannelStat(1);
     }
@@ -217,26 +183,22 @@ int func_ASearchSndTbl( int idx )
   int i = idx+1;
   int srch_cnt = 0;
 
-  if(i == g_maxterm_cnt)
-  {
+  if(i == g_maxterm_cnt) {
     i = 0;
   }
 
 RESEARCH:
   for(; i<g_maxterm_cnt; i++)
   {
-    if(g_TermTable[i].onuse == 0)
-    {
-      if(i == idx)
-      {
+    if(g_TermTable[i].onuse == 0) {
+      if(i == idx) {
         // 한번 씩 확인후는 오류처리
         return -1;
       }
-      if(i == g_maxterm_cnt)
-      {
+
+      if(i == g_maxterm_cnt) {
         i = 0;
-        if(srch_cnt > 1)
-        {
+        if(srch_cnt > 1) {
           return -1;
         }
         srch_cnt++;
@@ -244,9 +206,7 @@ RESEARCH:
         goto RESEARCH;
       }
       continue;
-    }
-    else
-    {
+    } else {
       return i;
     }
   }
@@ -256,21 +216,18 @@ RESEARCH:
 
 int	func_GetSGFirstIdx(char* SGName)
 {
-  //int idx=0;
   int i;
 
   for(i=0; i<g_maxterm_cnt; i++)
   {
-    if( strcmp(g_TermTable[i].SGName, SGName) == 0)
-    {
+    if( strcmp(g_TermTable[i].SGName, SGName) == 0) {
       return i;
     }
   }
 
   for(i=0; i<g_maxterm_cnt; i++)
   {
-    if(g_TermTable[i].onuse == 0)
-    {
+    if(g_TermTable[i].onuse == 0) {
       return i;
     }
   }
@@ -283,8 +240,7 @@ int func_SearchIdxByStidx(int stidx)
   int i;
   for(i=0; i<g_maxterm_cnt; i++)
   {
-    if(g_TermTable[i].stidx == stidx)
-    {
+    if(g_TermTable[i].stidx == stidx) {
       return i;
     }
   }
@@ -299,20 +255,7 @@ int func_ASearchEmptyTbl()
 
   for(i=0; i<g_maxterm_cnt; i++)
   {
-    if(g_TermTable[i].onuse == 0)
-    {
-#if 0
-      g_TermTable[i].onuse		= 2;
-      g_TermTable[i].stidx		= -1;
-      g_TermTable[i].cnt_stat		= -1;
-
-      g_TermTable[i].PeerIP		= 0;
-      g_TermTable[i].PeerPort		= 0;
-      g_TermTable[i].LocalPort	= 0;
-
-      memset( g_TermTable[i].SGName, 0x00,
-          sizeof(g_TermTable[i].SGName) );
-#endif
+    if(g_TermTable[i].onuse == 0) {
       return i;
     }
   }
@@ -341,19 +284,6 @@ int func_ASearchSessTbl(int LocalPort, int PeerPort)
 
 void func_AClearTable(int idx)
 {
-  // close
-#if 0
-  int nRc; 
-  nRc = rsof_resetFD(g_TermTable[idx].stidx, _IGNORE, _IGNORE);
-  if(nRc < 0)
-  {
-    ulog(_ERROR_, "Session Close Fail.(%d)", nRc);
-  }
-
-  ulog(_FLOW_, "Session Close idx:%d\n", idx);
-#endif
-
-
   // table clear
   g_TermTable[idx].stidx		= -1;
   g_TermTable[idx].onuse		= 0;
@@ -363,14 +293,7 @@ void func_AClearTable(int idx)
   g_TermTable[idx].PeerPort	= 0;
   g_TermTable[idx].LocalPort	= 0;
 
-
-  memset( g_TermTable[idx].SGName, 0x00, 
-      sizeof(g_TermTable[idx].SGName) );
-
-
-  //    g_TermTable[idx].poll_cnt	= 0;
-  //	g_TermTable[idx].sndcnt		= 0;
-  //	g_TermTable[idx].rcvcnt		= 0;
+  memset( g_TermTable[idx].SGName, 0x00, sizeof(g_TermTable[idx].SGName) );
 
   return;
 }
@@ -381,20 +304,15 @@ int func_AGetFd(int idx, int* Fd)
   int Len, nRc;
   int stidx = g_TermTable[idx].stidx;
 
-  if (stidx < 0)
-  {
+  if (stidx < 0) {
     ulog(_WARNING_, "[로그정보] stidx 값이 0보다 작음" );
-
     return -1;
   }
 
-
   Len = sizeof(int);
   nRc = rsof_getSessionInfo(stidx, _IGNORE, _IGNORE, RSOF_FDNUM, Fd, &Len);
-  if( nRc < 0 )
-  {
+  if( nRc < 0 ) {
     ulog(_WARNING_, "[로그정보] FD 값이 취득 실패" );
-
     return -3;
   }
 
@@ -405,25 +323,19 @@ int func_AGetFd(int idx, int* Fd)
 
 int	func_SendChannelStat(int stat)
 {
-
   int				nRc;
-
   S_CL_HEADER		*pHdr=NULL;
-
   char*	pSndMsg=NULL;
   int		SndMsgLen=0;
 
-
-  if(g_SessMonitor == 0 )
-  {
+  if(g_SessMonitor == 0 ) {
     //		ulog(_FLOW_, "회선관리 대상이 아님" ); 
     return 0;
   }
 
   SndMsgLen = sizeof(S_CL_HEADER);
   pSndMsg = tpalloc("CARRAY", NULL, SndMsgLen);
-  if(pSndMsg == NULL)
-  {
+  if(pSndMsg == NULL) {
     ulog(_ERROR_, "[로그정보] tpalloc fail (size:%d))" , SndMsgLen);
     return -1;
   }
@@ -433,33 +345,25 @@ int	func_SendChannelStat(int stat)
   pHdr = (S_CL_HEADER *)pSndMsg;
 
   /*InHead 설정 */
-  if(stat == 1)
-  {
-    memcpy( pHdr->c_MsgDsc, DEF_MSG_DSC_UPCHANNEL
-        , strlen(DEF_MSG_DSC_UPCHANNEL));
-  }
-  else
-  {
-    memcpy( pHdr->c_MsgDsc, DEF_MSG_DSC_DOWNCHANNEL
-        , strlen(DEF_MSG_DSC_DOWNCHANNEL));
+  if(stat == 1) {
+    memcpy( pHdr->c_MsgDsc, DEF_MSG_DSC_UPCHANNEL, strlen(DEF_MSG_DSC_UPCHANNEL));
+  } else {
+    memcpy( pHdr->c_MsgDsc, DEF_MSG_DSC_DOWNCHANNEL, strlen(DEF_MSG_DSC_DOWNCHANNEL));
   }
   memcpy( pHdr->c_BeChanID, g_VANCode, strlen(g_VANCode) );
   memcpy( pHdr->c_ExChanID, g_VANCode, strlen(g_VANCode) );
 
   /* 파일 수신 처리 서비스로 전송 */
   nRc = tpacall(g_APSvc, pSndMsg, SndMsgLen, TPNOREPLY | TPBLOCK);
-  if(nRc < 0)
-  {
+  if(nRc < 0) {
     tpfree(pSndMsg);
     ulog(_ERROR_, "[로그정보] 송신 실패 to [%s] (nRc:%d)", g_APSvc, nRc);
     return -1;
-  }
-  else
-  {
-    roTrace('O', g_APSvc, SndMsgLen, (unsigned char*)pSndMsg);
-    tpfree(pSndMsg);
-    ulog(_FLOW_, "[로그정보] 송신 성공 to %s", g_APSvc);
-  }	
+  } 
+
+  roTrace('O', g_APSvc, SndMsgLen, (unsigned char*)pSndMsg);
+  tpfree(pSndMsg);
+  ulog(_FLOW_, "[로그정보] 송신 성공 to %s", g_APSvc);
 
   return 0;
 }
