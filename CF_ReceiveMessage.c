104778a069028811c3d87df08dc26fcda1a48ea4
Byounghoon Lee <lbspresley@gmail.com>
2025-10-20 19:10:32 +0900

    remove length field in data received (remove at rmp config)

--- CF_ReceiveMessage.c
+++ CF_ReceiveMessage.c
@@ -10,10 +10,6 @@ int CF_ReceiveMessage(int bufkind, unsigned char** ppFrame,
   int inlen = *pFrameLen;
   int rc = 0;
   
-  // remove length (5) field
-  in = in + 5;
-  inlen = inlen - 5;
-
   /*
     1. 암호화 여부 확인
       1. plain text(x) or encrypted(o)
@@ -57,3 +53,4 @@ int CF_ReceiveMessage(int bufkind, unsigned char** ppFrame,
 
   return RC_NEXT_ACTION;
 }
+
